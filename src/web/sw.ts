df790917c4c66f7e956089556c9a5863657570df
Jon Nyman <nymanjon@gmail.com>
2023-06-25 16:58:57 -0700

    Remove unneeded code

--- src/web/sw.ts
+++ src/web/sw.ts
@@ -4,7 +4,6 @@ import "./service-worker/routes.js"
 import links from "./entry-points.js"
 
 self.addEventListener("install", async (e: Event) => {
-    if (await caches.has(version)) return
     // @ts-ignore
     e.waitUntil(
         caches.open(version)
